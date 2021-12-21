8164939bba5485aa73560ff36f66ff65460d1c25
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-12-21 00:59:05 +0000

    final app

---
+++ src/views/EventList.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Events for good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
